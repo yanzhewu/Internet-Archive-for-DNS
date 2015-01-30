e8afd7174e1fbd0c1f91ca18e6fcc7b528fd8761
wuyanzhe <yanzhewu@uchicago.edu>
2015-01-30 13:43:51 -0600

    gitignore

---
+++ Storage_17/src/HistoryCheck.java
@@ -0,0 +1,10 @@
+/**
+ * Created by wuyanzhe on 1/30/15.
+ */
+// We can transform the initial record to organized ones
+public class HistoryCheck {
+    private store store;
+    public static void historyCheck(String server){
+        
+    }
+}
--- Storage_17/src/SerialServer.java
+++ Storage_17/src/SerialServer.java
@@ -5,59 +5,85 @@ import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 
-public class SerialServer {
-    public static void main(String[] args) throws IOException
-    {
+public class SerialServer extends Thread{
+    protected static boolean serverContinue = true;
+    protected Socket clientSocket;
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(10007);
-        }
-        catch (IOException e)
-        {
-            System.err.println("Could not listen on port: 10007.");
-            System.exit(1);
-        }
-
-        Socket clientSocket = null;
-
-        try {
-            System.out.println ("Waiting for Client");
-            clientSocket = serverSocket.accept();
-        }
-        catch (IOException e)
-        {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-
-        ObjectOutputStream out = new ObjectOutputStream(
-                clientSocket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(
-                clientSocket.getInputStream());
-
-        ArrayList<record> list = null;
-
-        try {
-            list = (ArrayList<record>) in.readObject();
-            for(record record:list){
-                store.store(record,record.getServer());
+        if(args.length==0) {
+            try {
+                serverSocket = new ServerSocket(10007);
+                try {
+                    while (serverContinue) {
+                        serverSocket.setSoTimeout(600000);
+                        System.out.println("Waiting for Client");
+                        try {
+                            new SerialServer(serverSocket.accept());
+                        } catch (SocketTimeoutException ste) {
+                            System.out.println("Timeout Occurred.");
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
+                }
+            } catch (IOException e) {
+                System.err.println("Could not listen on port: 10007.");
+                System.exit(1);
+            } finally {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    System.out.println("Could not close port: 10007");
+                    System.exit(1);
+                }
             }
         }
-        catch (Exception ex)
-        {
-            System.out.println (ex.getMessage());
+        else{
+            if(args[0].equalsIgnoreCase("show")){
+                HistoryCheck.historyCheck(args[1]);
+            }
         }
+    }
+
+    private SerialServer (Socket clientSoc){
+        this.clientSocket = clientSoc;
+        start();
+    }
+
+    public void run(){
+        System.out.println("New TCP connection thread started.");
+        try{
+            ObjectOutputStream out = new ObjectOutputStream(
+                    clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(
+                    clientSocket.getInputStream());
+
+            ArrayList<record> list = null;
+
+            try {
+                list = (ArrayList<record>) in.readObject();
+                for(record record:list){
+                    store.store(record,record.getServer());
+                }
+            }
+            catch (Exception ex)
+            {
+                System.out.println (ex.getMessage());
+            }
 
 
-        System.out.println ("Server recieved point: " + list + " from Client");
+            System.out.println ("Server recieved point: " + list + " from Client");
 
-        out.flush();
+            out.flush();
 
 
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
+            out.close();
+            in.close();
+            clientSocket.close();
+        }catch (IOException e){
+            System.err.println("Server error.");
+            System.exit(1);
+        }
     }
 }
